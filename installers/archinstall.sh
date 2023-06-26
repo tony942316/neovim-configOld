d0c9ad8486c3a16f05e3458ff9adcd9cbafb26ed
tony942316 <grassoa159@gmail.com>
2023-06-26 10:28:34 -0400

    Arch Installer

---
+++ installers/archinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman -S git zip unzip wget curl tree \
+    make cmake gcc clang python python-pynvim \
+    nodejs npm rust ripgrep neovim
+
+sudo npm install -g npm@latest
+sudo npm i -g neovim
+
+git clone https://github.com/tony942316/neovim-config.git \
+    ~/.config/nvim
